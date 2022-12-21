0b65d5533a50946344affa8716a9082a1a0f2755
smxl <92101931+smxl@users.noreply.github.com>
2022-12-21 18:43:55 +0800

    Create yt.sponsor.skip.user.js

---
+++ yt.sponsor.skip.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         YouTube Sponsor Skip
+// @namespace    https://github.com/smxl
+// @version      1.0.0
+// @description  YouTube Sponsor Skip
+// @author       smxl
+// @match        *://*.youtube.com/*
+// @exclude      *://*.youtube.com/subscribe_embed?*
+// ==/UserScript==
+const tryFetchSkipSegments = async (videoID) => {
+  try {
+    const response = await fetch(`https://sponsor.ajay.app/api/skipSegments?videoID=${videoID}`);
+    const rJson = await response.json();
+    const segments = [];
+    rJson.forEach((a) => {
+      if (a.actionType === 'skip') {
+        segments.push(a.segment);
+      }
+    });
+    return segments;
+  } catch (e) {
+    console.log(`Sponsor Skip: failed fetching skip Segments for ${videoID}, reason: ${e}`);
+    return [];
+  }
+};
+
+const skipSegments = () => {
+  const url = new window.URL(document.location);
+  const params = new window.URLSearchParams(url.search);
+  const videoID = params.get('v');
+  if (!videoID) {
+    return;
+  }
+  const key = `segmentsToSkip-${videoID}`;
+  window[key] = window[key] || tryFetchSkipSegments(videoID);
+  document.querySelectorAll('video').forEach((v) => {
+    if (Number.isNaN(v.duration)) return;
+    window[key].forEach(([start, end]) => {
+      if (v.currentTime < end && v.currentTime > start) {
+        v.currentTime = end;
+        console.log(`Sponsor Skip: skipped video to ${end}`);
+      }
+    });
+  });
+};
+
+document.querySelectorAll('video').forEach((v) => {
+  v.addEventListener('timeupdate', skipSegments);
+});
